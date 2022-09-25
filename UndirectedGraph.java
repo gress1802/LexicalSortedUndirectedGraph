75bbda6c1efed8ea6a1b19facd2e619e7a7f071e
kaajc <jgress1802@gmail.com>
2022-09-25 17:19:34 -0500

    Potentially fixed addEdge() method

--- UndirectedGraph.java
+++ UndirectedGraph.java
@@ -99,11 +99,67 @@ public class UndirectedGraph {
 
         //if either of the Strings are the same as the vertices String then we have to first add an edge to the vertices node in the correct order
         //after that we need to update the second edge list
-        if(v1.equals(vertices.vertexName)){
-            vertexForString1.edges[0] = new EdgeNode(vertexForString2, vertexForString1);
-        }else if(v2.equals(vertices.vertexName)){
-            vertexForString2.edges[0] = new EdgeNode(vertexForString1, vertexForString2);
+
+        //This takes care of all cases where the vertices is v1 or v2
+        if(v1.equals(vertices.vertexName)){ //if v1 is the vertex then we need to *add v2 to vertexForString1.edges[0]* and *add v1 to vertexForString2.edges[1]*
+            insertSortedEdge(vertexForString1,vertexForString2,v1,v2); //inserts into edges[0] list
+        }else if(v2.equals(vertices.vertexName)){ //if v2 is the vertex then we need to *add v1 to vertexForString2.edges[0]* and then *add v2 to vertexForString1.edges[1]*
+            insertSortedEdge(vertexForString2, vertexForString1, v2, v1); //inserts into edges[0] list
+        }else{//Every other case for B,C,D,E,F
+           if(v1.compareToIgnoreCase(v2) < 0){ // this is the normal case where v1 is less than v2
+                if(vertexForString1.edges[0] == null || v1.compareToIgnoreCase(vertexForString1.edges[0].edge[0].vertexName) < 0){
+                    EdgeNode tempE = vertexForString1.edges[0];
+                    vertexForString1.edges[0] = new EdgeNode(vertexForString2,vertexForString1); // creating new edges[0] node
+                    vertexForString1.edges[0].nextE[0] = tempE; //adding back on the rest of the list 
+                    //now how edges[1] is updated
+                    insertEdge1(vertexForString1.edges[0], vertexForString1, vertexForString2, v1, v2);
+                    return;
+                }else{
+                    EdgeNode tempE = vertexForString1.edges[0];
+                    while(tempE.nextE[0] != null){
+                        if(v1.compareToIgnoreCase(tempE.nextE[0].edge[0].vertexName) < 0){
+                            EdgeNode tempp = tempE.nextE[0];
+                            tempE.nextE[0] = new EdgeNode(vertexForString2,vertexForString1);
+                            tempE.nextE[0].nextE[0] = tempp;
+                            //now how edges[1] is updated
+                            insertEdge1(tempE.nextE[0], vertexForString1, vertexForString2, v1, v2);
+                            return;
+                        }
+                    }
+                    tempE.nextE[0] = new EdgeNode(vertexForString2,vertexForString1);
+                    //edge[1] is updated
+                    insertEdge1(tempE.nextE[0], vertexForString1, vertexForString2, v1, v2);
+                }
+           }else{// this is when v2 is less than v1
+                if(vertexForString2.edges[0] == null || v2.compareToIgnoreCase(vertexForString2.edges[0].edge[0].vertexName) < 0){
+                    EdgeNode tempE = vertexForString2.edges[0];
+                    vertexForString2.edges[0] = new EdgeNode(vertexForString1,vertexForString2); // creating new edges[0] node
+                    vertexForString2.edges[0].nextE[0] = tempE; //adding back on the rest of the list 
+                    //now how edges[1] is updated
+                    insertEdge1(vertexForString2.edges[0], vertexForString2, vertexForString1, v2, v1);
+                    return;
+                }else{
+                    EdgeNode tempE = vertexForString2.edges[0];
+                    while(tempE.nextE[0] != null){
+                        if(v2.compareToIgnoreCase(tempE.nextE[0].edge[0].vertexName) < 0){
+                            EdgeNode tempp = tempE.nextE[0];
+                            tempE.nextE[0] = new EdgeNode(vertexForString1,vertexForString2);
+                            tempE.nextE[0].nextE[0] = tempp;
+                            //now how edges[1] is updated
+                            insertEdge1(tempE.nextE[0], vertexForString2, vertexForString1, v2, v1);
+                            return;
+                        }
+                    }
+                    tempE.nextE[0] = new EdgeNode(vertexForString1,vertexForString2);
+                    //edge[1] is updated
+                    insertEdge1(tempE.nextE[0], vertexForString2, vertexForString1, v2, v1);
+                }
+ 
+           }
         }
+
+        
+        
         
 
 
@@ -113,11 +169,62 @@ public class UndirectedGraph {
         //Assume the edge has not been added
     }
 
+    //This method inserts an edgenode in the sorted list edges[0]. its parameters are the vertex and the 
+    public void insertSortedEdge(VertexNode vertexForString1, VertexNode vertexForString2, String v1, String v2){ 
+        //first adding v2 to vertexForString1.edges[0] list
+        EdgeNode temp1 = vertexForString1.edges[0];
+        if(temp1 == null || v2.compareToIgnoreCase(temp1.edge[0].vertexName)<0){ //This is the case where the head (edges[0]) needs to be changed
+            EdgeNode tempNext = vertexForString1.edges[0];
+            vertexForString1.edges[0] = new EdgeNode(vertexForString2,vertexForString1);
+            vertexForString1.edges[0].nextE[0] = tempNext;
+            insertEdge1(vertexForString1.edges[0], vertexForString1, vertexForString2, v1, v2); 
+            return;
+        }else{
+            while(temp1.nextE[0] != null){ //Otherwise traverse the list and look for the correct place for this edgenode
+                if(v2.compareToIgnoreCase(temp1.nextE[0].edge[0].vertexName)<0){
+                    EdgeNode tempNext = temp1.nextE[0];
+                    temp1.nextE[0] = new EdgeNode(vertexForString2,vertexForString1);
+                    temp1.nextE[0].nextE[0] = tempNext;
+                    insertEdge1(temp1.nextE[0], vertexForString1, vertexForString2, v1, v2); 
+                    return;
+                }
+                temp1 = temp1.nextE[0];
+            }//last case is if we are at the end of the list
+            temp1.nextE[0] = new EdgeNode(vertexForString2,vertexForString1);
+            insertEdge1(temp1.nextE[0], vertexForString1, vertexForString2, v1, v2);
+        } 
+        //now inserting into edges[1] list
+
+    }
+
+
+    //This method inserts edges[1] list. its parameter node is the edgenode that has already been added to the list
+    public void insertEdge1(EdgeNode node, VertexNode vertexForString1, VertexNode vertexForString2, String v1, String v2){ //This method adds edgenodes to edges[1] list
+        EdgeNode temp1 = vertexForString2.edges[1];
+        if(temp1 == null || v1.compareToIgnoreCase(temp1.edge[1].vertexName)<0){
+            vertexForString2.edges[1] = node;
+            return;
+        }else{
+            while(temp1.nextE[1] != null){ //Otherwise traverse the list and look for the correct place for this edgenode
+                if(v1.compareToIgnoreCase(temp1.nextE[1].edge[1].vertexName) < 0){
+                    EdgeNode tempNext = temp1.nextE[1];
+                    temp1.nextE[1] = node;
+                    node.nextE[1] = tempNext;
+                    return;
+                }
+                temp1 = temp1.nextE[1];
+            }
+            //This is the case where we have reached the end of the list
+            temp1.nextE[1] = node;
+        }
+    }
+
+
     public void printVertices(){
         String s = vertices.vertexName;
         VertexNode temp = vertices;
         while(temp.nextV!=null){
-            s = s+", "+temp.nextV.vertexName;
+            s = s+" "+temp.nextV.vertexName;
             temp = temp.nextV;
         }
         System.out.println(s);
@@ -125,18 +232,52 @@ public class UndirectedGraph {
 
     public void printGraph() {
        VertexNode temp = vertices;
-       EdgeNode tempE0 = temp.edges[0];
-       EdgeNode tempE1 = temp.edges[1];
-       System.out.println(temp.edges[0].edge[0].vertexName);
- /*        while(temp.nextV != null){//traversing through the list of vertices
-            tempE0 = temp.edges[0];
-            tempE1 = temp.edges[1];
-            while(tempE0.nextE != null){//traversing through the edges[0] list
+       EdgeNode tempE0;
+       EdgeNode tempE1;
 
-                tempE0 = tempE0.nextE[0];
+        EdgeNode tempFirst = vertices.edges[0];
+        System.out.print(vertices.vertexName); //printing the vertices
+        System.out.print(" "+tempFirst.edge[0].vertexName);//printing the first edgenode in the list [0]
+        while(tempFirst.nextE[0] != null){//prints the edges[0] of the first vertex (This vertex will have no edges[1])
+            System.out.print(" "+tempFirst.nextE[0].edge[0].vertexName);
+            tempFirst = tempFirst.nextE[0];
+        }
+
+        System.out.println();
+        System.out.println();
+
+
+        while(temp.nextV != null){//traverses through the vertices
+            if(temp.edges[0] == null){
+                System.out.println(temp.nextV.vertexName);
+            }else{
+                System.out.print(temp.nextV.vertexName);//printing the edges[0] list
+                tempE0 = temp.nextV.edges[0];
+
+                if(tempE0 == null){
+                    System.out.println();
+                }else{
+                    while(tempE0.nextE[0] != null){
+                        System.out.print(tempE0.nextE[0].edge[0].vertexName+" ");
+                        tempE0 = tempE0.nextE[0];
+                    }
+                    System.out.println();
+                }
+
             }
-       }*/
+            System.out.println();
+            temp = temp.nextV;
+        }
+
+
     }
+
+    public void test(){
+        VertexNode t = vertices.nextV;
+        System.out.println(t.edges[0].edge[0].vertexName);
+        
+    }
+
 
     public static void main(String args[]) throws IOException { 
         //DO NOT CHANGE main 
@@ -149,13 +290,19 @@ public class UndirectedGraph {
         g.addVertex("A");
         g.addVertex("B");
         g.addVertex("C");
-        g.printVertices();
 
+        g.addEdge("A", "D");
+        g.addEdge("A", "C");
+        g.addEdge("A", "F");
+        g.addEdge("A", "E");
         g.addEdge("A", "B");
-        g.addEdge("C","A");
-
+        g.addEdge("C", "B");        
+        g.addEdge("F","C");
+        g.addEdge("D", "C");
         g.printGraph();
+        
 
+        
 
         /* 
         UndirectedGraph g = new UndirectedGraph(); 
